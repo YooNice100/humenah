f7f714323b9e49d2ba2644c57b0c5f279c979a59
Eunice <eunicecho100@gmail.com>
2026-08-03 01:11:29 -0700

    added a console log to see completed habits by date

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,8 @@ function App() {
     );
   }, [completionsByDate]);
 
+  console.log(completionsByDate);
+
   const toggleHabit = (id) => {
     setCompletionsByDate((previousCompletions) => {
       const currentDateCompletions = previousCompletions[selectedDate] || {};
